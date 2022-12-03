da4358c65dd431c220859f641a27073f050bbd3f
Alexis Campomanes <alexiscp.developer@gmail.com>
2022-12-03 16:53:29 -0500

    nosotros

--- src/Footer/Nosotros/Nosotros.jsx
+++ src/Footer/Nosotros/Nosotros.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import { UilPlus } from '@iconscout/react-unicons'
 
 const Nosotros = () => {
     return (
         <div className='Nosotros'>
-          <div>
-            <div>
-              <span>sobre nosotros</span>
-              <UilPlus />
-            </div>
-            <ul>
-              <li>nuestra misión</li>
-              <li>tiendas, horarios y zona de cobertura</li>
-              <li>starbucks stories</li>
-            </ul>
-          </div>
+          
         </div>
     );
 }
